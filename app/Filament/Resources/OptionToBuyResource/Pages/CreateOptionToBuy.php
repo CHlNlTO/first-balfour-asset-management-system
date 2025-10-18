75e661b44152f05906c874eddab06bb7f7f123b9
Chinito <clark.wayne023@gmail.com>
2025-10-18 10:10:36 +0800

    [FIX-24] OptionToBuy Creation Should Update Assignment Status

--- app/Filament/Resources/OptionToBuyResource/Pages/CreateOptionToBuy.php
+++ app/Filament/Resources/OptionToBuyResource/Pages/CreateOptionToBuy.php
@@ -23,29 +23,34 @@ class CreateOptionToBuy extends CreateRecord
         // Get the created record
         $optionToBuy = $this->record;
 
-        // Get the assignment status name
-        $assignmentStatus = AssignmentStatus::find($optionToBuy->option_to_buy_status);
+        DB::beginTransaction();
+        try {
+            // Always update the assignment status to match the option to buy status
+            $optionToBuy->assignment->update([
+                'assignment_status' => $optionToBuy->option_to_buy_status
+            ]);
 
-        if ($assignmentStatus && in_array($assignmentStatus->assignment_status, ['Asset Sold', 'Option to Buy'])) {
-            // Try to find asset status by name, prioritizing "Asset Sold" then "Sold"
-            $soldAssetStatus = AssetStatus::where('asset_status', 'Asset Sold')->first()
-                ?? AssetStatus::where('asset_status', 'Sold')->first();
+            // Get the assignment status name to check if we need to update asset status
+            $assignmentStatus = AssignmentStatus::find($optionToBuy->option_to_buy_status);
 
-            if ($soldAssetStatus) {
-                // Update the asset status to the found sold status
-                DB::beginTransaction();
-                try {
+            // Only update asset status if the assignment status is "Asset Sold" or "Option to Buy"
+            if ($assignmentStatus && in_array($assignmentStatus->assignment_status, ['Asset Sold', 'Option to Buy'])) {
+                // Try to find asset status by name, prioritizing "Asset Sold" then "Sold"
+                $soldAssetStatus = AssetStatus::where('asset_status', 'Asset Sold')->first()
+                    ?? AssetStatus::where('asset_status', 'Sold')->first();
+
+                if ($soldAssetStatus) {
+                    // Update the asset status to the found sold status
                     $optionToBuy->assignment->asset->update([
                         'asset_status' => $soldAssetStatus->id
                     ]);
-
-                    DB::commit();
-                } catch (\Exception $e) {
-                    DB::rollBack();
-                    throw $e;
                 }
             }
-            // If neither "Asset Sold" nor "Sold" status exists, do nothing
+
+            DB::commit();
+        } catch (\Exception $e) {
+            DB::rollBack();
+            throw $e;
         }
     }
 }
